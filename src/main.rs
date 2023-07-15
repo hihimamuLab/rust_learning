93276494feddfdb48afd481eb769c8bd9ffc6f7a
hihimamuLab <hihimamu14@outlook.jp>
2023-07-15 14:32:11 +0900

    learned vect

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,23 @@ v.push(5);
 v.push(6);
 v.push(7);
 
-println!("{:?}", v)
+#[derive(Debug)]
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+let row = vec![
+    SpreadsheetCell::Int(3),
+    SpreadsheetCell::Float(10.22),
+    SpreadsheetCell::Text(String::from("blue")),
+];
+
+match &row[2] {
+    SpreadsheetCell::Int(first) => println!("{}", first),
+    SpreadsheetCell::Float(second) => println!("{}", second),
+    SpreadsheetCell::Text(third) => println!("{}", third), 
+}
 }
 
