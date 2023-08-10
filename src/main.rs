10286ab6245271c69b7b554d50f00b950099500b
hihimamuLab <hihimamu14@outlook.jp>
2023-08-10 22:43:03 +0900

    aoj beta: Print a Rectangle

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,33 @@ use std::io;
 
 fn main() {
     loop {
-        let mut formula_str: String = String::new();
-        io::stdin()
-            .read_line(&mut formula_str)
-            .expect("Error");
-        let formula: Vec<&str> = formula_str
-            .split_whitespace()
-            .collect();
-        let num_1: i32 = formula[0].parse().unwrap();
-        let num_2: i32 = formula[2].parse().unwrap();
-        if formula[1] == "+" {
-            println!("{}", num_1 + num_2);
-        } else if formula[1] == "-" {
-            println!("{}", num_1 - num_2);
-        } else if formula[1] == "*" {
-            println!("{}", num_1 * num_2);
-        } else if formula[1] == "/" {
-            println!("{}", num_1 / num_2);
-        } else if formula[1] == "?" {
+        let size: Vec<i32> = read_buffer();
+        if size == [0, 0] {
             break;
-        } 
+        }else {
+            draw_rectangle(size);     
+        }
+    }
+}
+
+fn read_buffer() -> Vec<i32> {
+    let mut buffer: String = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Error");
+    buffer.trim()
+        .split_whitespace()
+        .map(|x| x.parse().expect("Error"))
+        .collect()
+}
+
+fn draw_rectangle(size_vec: Vec<i32>) {
+    let grid: Vec<Vec<&str>> = vec![vec!["#"; size_vec[1].try_into().unwrap()]; size_vec[0].try_into().unwrap()];
+    
+    for row in grid {
+        for cell in row {
+            print!("{}", cell);
+        }
+        println!();
     }
 }
