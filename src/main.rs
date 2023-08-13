6500ec7576241f49aa741c40218242096eed8bf1
hihimamuLab <hihimamu14@outlook.jp>
2023-08-13 23:33:36 +0900

    aoj beta: Print a Frame

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,22 @@
 use std::io;
-
 fn main() {
     loop {
-        let size: Vec<i32> = read_buffer();
-        if size == [0, 0] {
-            break;
-        }else {
-            draw_rectangle(size);     
-        }
+        let size: Vec<usize> = read_buffer();
+        let (hight, width): (usize, usize) = (size[0], size[1]);
+        if hight == 0 && width == 0 { break; }
+        let row_str: String = String::from("#").repeat(width) + "\n";
+        let line_str: String = format!("{}{}{}\n", "#", String::from(".").repeat(width - 2), "#");
+        println!("{}{}{}", row_str, line_str.repeat(hight - 2), row_str);
     }
 }
 
-fn read_buffer() -> Vec<i32> {
+fn read_buffer() -> Vec<usize> {
     let mut buffer: String = String::new();
     io::stdin()
         .read_line(&mut buffer)
         .expect("Error");
     buffer.trim()
         .split_whitespace()
-        .map(|x| x.parse().expect("Error"))
+        .map(|x| x.parse::<usize>().expect("Error"))
         .collect()
-}
-
-fn draw_rectangle(size_vec: Vec<i32>) {
-    let grid: Vec<Vec<&str>> = vec![vec!["#"; size_vec[1].try_into().unwrap()]; size_vec[0].try_into().unwrap()];
-    
-    for row in grid {
-        for cell in row {
-            print!("{}", cell);
-        }
-        println!();
-    }
 }
