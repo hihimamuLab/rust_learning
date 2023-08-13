041ba64b2d92a202380656608f664a29cda69b05
hihimamuLab <hihimamu14@outlook.jp>
2023-08-13 23:57:26 +0900

    aoj beta: Print a Chessboard

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,60 @@
-use std::io;
-fn main() {
-    loop {
-        let size: Vec<usize> = read_buffer();
-        let (hight, width): (usize, usize) = (size[0], size[1]);
-        if hight == 0 && width == 0 { break; }
-        let row_str: String = String::from("#").repeat(width) + "\n";
-        let line_str: String = format!("{}{}{}\n", "#", String::from(".").repeat(width - 2), "#");
-        println!("{}{}{}", row_str, line_str.repeat(hight - 2), row_str);
+use std::io::*;
+use std::str::FromStr;
+
+struct Scanner<R: Read> {
+    reader: R,
+}
+
+#[allow(dead_code)]
+impl<R: Read> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner { reader: reader }
+    }
+
+    fn safe_read<T: FromStr>(&mut self) -> Option<T> {
+        let token = self.reader.by_ref().bytes().map(|c| c.unwrap() as char)
+            .skip_while(|c| c.is_whitespace())
+            .take_while(|c| !c.is_whitespace())
+            .collect::<String>();
+        if token.is_empty() {
+            None
+        } else {
+            token.parse::<T>().ok()
+        }
+    }
+
+    fn read<T: FromStr>(&mut self) -> T {
+        if let Some(s) = self.safe_read() {
+            s
+        } else {
+            writeln!(stderr(), "Terminated with EOF").unwrap();
+            std::process::exit(0);
+        }
     }
 }
 
-fn read_buffer() -> Vec<usize> {
-    let mut buffer: String = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Error");
-    buffer.trim()
-        .split_whitespace()
-        .map(|x| x.parse::<usize>().expect("Error"))
-        .collect()
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    loop {
+        let h: u32 = sc.read();
+        let w: u32 = sc.read();
+        if h == 0 { break }
+        for i in 0..h {
+            for j in 0..w {
+                let c = if (i + j) % 2 == 0 {
+                    '#'
+                } else {
+                    '.'
+                };
+                print!("{}", c);
+            }
+            println!("");
+        }
+        println!("");
+    }
 }
+
+
