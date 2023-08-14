4191c03bd0bfd7856bb6dd393ff16402f1c426d4
hihimamuLab <hihimamu14@outlook.jp>
2023-08-15 08:56:45 +0900

    aoj beta: Finding Missing Cards

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+use std::{io, collections::HashMap};
+
+const SUITS: &str = "SHCD";
+
 fn main() {
-    let mut buf1: String = String::new();
-    std::io::stdin().read_line(&mut buf1).expect("error");
-    let mut buf2 = String::new();
-    std::io::stdin().read_line(&mut buf2).expect("error");
+    let mut cards = HashMap::new();
 
-    let seq_a_rev: Vec<&str> = buf2.split_whitespace().rev().collect();
-    let ans = seq_a_rev.join(" ");
+    let mut buf1 = String::new();
+    io::stdin()
+        .read_line(&mut buf1)
+        .expect("error");
+    let n: usize = buf1.trim().parse().unwrap();
 
-    println!("{}", ans);
+    for _ in 0..n {
+        let mut buf2 = String::new();
+        io::stdin()
+            .read_line(&mut buf2)
+            .expect("error");
+        let card: String = buf2.trim().to_string();
+        cards.insert(card, true);
+    }
+
+    for suit in SUITS.chars() {
+        for rank in 1..=13 {
+            let card = format!("{} {}", suit, rank);
+            if cards.get(&card) == None {
+                println!("{}", card);
+            }
+        }
+    }
 }
